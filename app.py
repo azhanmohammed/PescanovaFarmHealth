298b04017afaab73f69b75ac846e9416c7840942
Azhan Mohammed <46521547+sheikhazhanmohammed@users.noreply.github.com>
2023-01-04 15:22:47 +0530

    st map

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from utils import popupTable
 import branca
 
 
-map = folium.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True)
-st_map = st_folium(map, width=1000, height=650)
+map = st.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True)
+# st_map = st_folium(map, width=1000, height=650)
 
 
 # dataframe = gpd.read_file("./static/pescanovaGeoJsonWithProperties.geojson")
