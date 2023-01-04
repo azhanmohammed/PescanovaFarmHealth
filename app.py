ea9c10c9bb83839441e80ca94899dfadd42d5a90
Azhan Mohammed <46521547+sheikhazhanmohammed@users.noreply.github.com>
2023-01-04 15:12:24 +0530

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ dataframe = gpd.read_file("./static/pescanovaGeoJsonWithProperties.geojson")
 
 # Specify the title and logo for the web page.
 st.set_page_config(page_title='Pond Health Analysis for Neuva Pescanova', page_icon='./static/galaxeye.png', layout="wide")
-map = folium.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True)
+
 # Add a sidebar to the web page. 
 with st.sidebar:
     choose = option_menu("Pond Health Analysis for Neuva Pescanova", ["Dashboard","Chlorophyll","Dissolved Oxygen","pH","Salinity","Turbidity","Custom Filter"],
@@ -34,22 +34,24 @@ if choose == "Dashboard":
         font-size:35px ; font-family: 'Cooper Black'; color: #ffffff;} 
         </style> """, unsafe_allow_html=True)
         st.markdown('<p class="font">Detailed Analysis</p>', unsafe_allow_html=True)    
-    with col2:               # To display brand log
+    with col2:
         st.image(logo, width=130 )
-    # img = folium.raster_layers.ImageOverlay(
-    #     name="RGB Image",
-    #     image="./static/rgbimage.png",
-    #     bounds=[[-2.460891311086019, -79.99976724386217], [-2.4354694248845985, -79.96761258691551]],
-    #     opacity=1.0,
-    #     interactive=True,
-    #     cross_origin=False,
-    #     zindex=1,
-    # )
+    
+    map = folium.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True, tiles='CartoDB positron')
+    img = folium.raster_layers.ImageOverlay(
+        name="RGB Image",
+        image="./static/rgbimage.png",
+        bounds=[[-2.460891311086019, -79.99976724386217], [-2.4354694248845985, -79.96761258691551]],
+        opacity=1.0,
+        interactive=True,
+        cross_origin=False,
+        zindex=1,
+    )
 
-    # img.add_to(map)
-    # for l, row in enumerate(dataframe.iterrows()):
-    #         html = popupTable(dataframe, l)
-    #         iframe = branca.element.IFrame(html=html,width=700,height=600)
-    #         popup = folium.Popup(folium.Html(html, script=True), max_width=500)
-    #         folium.GeoJson(data=row[1][5], popup=popup).add_to(map)
+    img.add_to(map)
+    for l, row in enumerate(dataframe.iterrows()):
+            html = popupTable(dataframe, l)
+            iframe = branca.element.IFrame(html=html,width=700,height=600)
+            popup = folium.Popup(folium.Html(html, script=True), max_width=500)
+            folium.GeoJson(data=row[1][5], popup=popup).add_to(map)
     st_map = st_folium(map, width=1000, height=650)
